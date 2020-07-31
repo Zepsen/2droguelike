17f46b749a150373a9178a70debfc6b4ae27b32f
Dmytro Zepsen <Dmytro.Zepsen@refinitiv.com>
2020-07-31 12:34:53 +0300

    generation level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager board;
+    
+    private int level = 3;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;            
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        board = GetComponent<BoardManager>();
+        InitGame();   
+    }
+
+    private void InitGame()
+    {
+        board.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
